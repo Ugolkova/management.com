252c7eb50e93ecb249b4fc2990f77a5d6883e71c
Tetiana Ugolkova <tugolkova@cogniance.com>
2015-08-18 20:11:17 +0300

    Added pagination and .tableSubmit block

--- views/users/list.php
+++ views/users/list.php
@@ -20,9 +20,28 @@
                         <option value="list">All Users</option>
                         <option value="lm_list">LM Users</option>
                         <option value="pm_list">PM Users</option>
+                    </select>
+                    
+                    <select name="results_count">
+                        <option value="10">10 results</option>
+                        <option value="20">20 results</option>
+                        <option value="50">50 results</option>
+                        <option value="100">100 results</option>
+                        <option value="150">150 results</option>
                     </select>    
                 </fieldset>
             </form>    
+            
+            <div class="pagination">
+                <div class="prev"></div>
+                <div class="next"></div>
+                <ul>
+                    <li><a href="#1">1</a></li>
+                    <li><strong>2</strong></li>
+                    <li><a href="#3">3</a></li>
+                    <div class="clear"></div>
+                </ul>
+            </div>
             
             <form action="<?php echo URL . "edit/" ?>" method="POST">
                 <table>
@@ -61,6 +80,25 @@
                         </tr>
                     </tbody>
                 </table>
+                
+                <div class="pagination">
+                    <div class="prev"></div>
+                    <div class="next"></div>
+                    <ul>
+                        <li><a href="#1">1</a></li>
+                        <li><strong>2</strong></li>
+                        <li><a href="#3">3</a></li>
+                        <div class="clear"></div>
+                    </ul>
+                </div>
+                
+                <div class="tableSubmit">
+                    <input type="submit" name="submit" value="Submit" />
+                    <select name="action">
+                        <option value="edit">edit selected</option>
+                        <option value="delete">delete selected</option>
+                    </select>    
+                </div>    
             </form>
         </section>    
     </body>
