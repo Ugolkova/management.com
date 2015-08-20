b2e6057712faf74f36dc031ef7ebf4be60b39cd9
Tetiana Ugolkova <tugolkova@cogniance.com>
2015-08-20 10:48:46 +0300

    $sql users get_list

--- models/users_model.php
+++ models/users_model.php
@@ -6,5 +6,17 @@ class Users_model extends Model {
         parent::__construct();
     }
 
+    public function full_list(){
+        //$this->db->select("SELECT ")
+    }
+    
+    public function get_list(){
+        $sql = "SELECT t1.user_id, t1.user_name, t2.lm_id, t3.user_name as lm_name, t4.pm_id, t5.user_name as pm_name
+        FROM users as t1
+        LEFT JOIN lm_users as t2 ON t2.user_id = t1.user_id 
+        LEFT JOIN users as t3 ON t3.user_id = t2.lm_id
+        LEFT JOIN pm_users as t4 ON t4.user_id = t1.user_id 
+        LEFT JOIN users as t5 ON t5.user_id = t4.pm_id";
+    }
 }
 
