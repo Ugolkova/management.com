03f1378ac75c9e37309177d49102981e2f3b7fa4
root <ugolkova.t@gmail.com>
2015-08-24 18:59:43 +0300

    Pagination class is ready

--- libs/Pagination.php
+++ libs/Pagination.php
@@ -1,132 +1,63 @@
 <?php
 
 class Pagination {
-    private $_countEntriesOnPage = 5;
-    private $_prevLinkTitle = 'Prev';
-    private $_nextLinkTitle = 'Next';
-    private $_limit = 9;
-    private $_url = '';
+    /**
+     * Number of links before and after current element
+     * @var int 
+     */
+    private $_interval = 2;
+    /**
+     * Items per page
+     * @var int 
+     */
+    private $_perPage = 3;
+    private $_prevLink = '';
+    private $_nextLink = '';
+    private $_firstLink = '';
+    private $_lastLink = '';
     
-    function __construct() {
-        echo $this->createLinks($_GET['page'], 13, '/test.php');
-    }
     
-    public function createLinks($currentPageNum, $totalRows, $url){
-        $this->_url = $url;
+    public function createLinks($curNum, $link, $totalRows){
+        $totalNum = (int) ceil($totalRows / $this->_perPage);
+        $html = '';
         
-        $pageCount = ceil($totalRows / $this->_countEntriesOnPage);
-        $html = "";
+        $start = $curNum - $this->_interval;
+        if($start < 1){
+            $start = 1;
+        }
         
-        $countLinks = ($pageCount > $this->_limit) ? $this->_limit : $pageCount;
+        $end = $curNum + $this->_interval;
+        if($end > $totalNum){
+            $end = $totalNum;
+        }    
         
-        // First Page
-        if($currentPageNum == 1){
-            $html .= $this->_setCurrentLink(1);            
-            for($i = 2; $i <= $countLinks; $i++){
-                $html .= $this->_setSimpleLink($i);
-            }
-
-            if($pageCount > $countLinks){
-                $html .= $this->_setEllipsis();
-                $html .= $this->_setSimpleLink($pageCount);
-            }
-            
-            if($currentPageNum != $totalRows){
-                $html .= $this->_setNextLink($currentPageNum + 1);
-            }
-        } 
-        // Last Page
-        else if($currentPageNum == $pageCount) {
-            $html .= $this->_setPrevLink($currentPageNum - 1); 
-            $html .= $this->_setEllipsis();
-            
-            $start = ($pageCount - $countLinks) > 1 ? ($pageCount - $countLinks) : 1;
-            for($i = $start; $i <= $pageCount; $i++){
-                if($i == $currentPageNum){
-                    $html .= $this->_setCurrentLink($i);
-                } else {
-                    $html .= $this->_setSimpleLink($i);
-                }
+        if($start > 1){
+            $html .= '<a href="' . $link . '1" class="first">' . $this->_firstLink . '</a>';
+        }
+        if($curNum > 1){
+            $html .= '<a href="' . $link . ($curNum - 1) . '" class="prev">' . $this->_prevLink . '</a>';
+        }
+        
+        for($i = $start; $i <= $end; $i++){
+            if($i == $curNum){
+                $html .= '<strong>' . $i . '</strong>';
+            } else {
+                $html .= '<a href="' . $link . $i . '">' . $i . '</a>';
             }
         }
-        // First pages
-        else if($currentPageNum < $this->_limit){
-            $html .= $this->_setPrevLink($currentPageNum - 1); 
-            
-            for($i = 1; $i <= $countLinks; $i++){
-                if($i == $currentPageNum){
-                    $html .= $this->_setCurrentLink($i);
-                } else {
-                    $html .= $this->_setSimpleLink($i);
-                }
-            }
-
-            if($pageCount > $countLinks){
-                $html .= $this->_setEllipsis();
-                $html .= $this->_setSimpleLink($pageCount);
-            }
-            
-            if($currentPageNum != $totalRows){
-                $html .= $this->_setNextLink($currentPageNum + 1);
-            }            
+        
+        if($curNum < $totalNum){
+            $html .= '<a href="' . $link . ($curNum + 1) . '" class="next">' . $this->_nextLink . '</a>';
         }
-        // Last pages
-        else if($currentPageNum > ($pageCount - $this->_limit +1)){
-            $html .= $this->_setPrevLink($currentPageNum - 1); 
-            $html .= $this->_setSimpleLink(1);
-            $html .= $this->_setEllipsis();
-            
-            for($i = ($pageCount - $this->_limit + 1); $i <= $pageCount; $i++){
-                if($i == $currentPageNum){
-                    $html .= $this->_setCurrentLink($i);
-                } else {
-                    $html .= $this->_setSimpleLink($i);
-                }
-            }
-
-            if($currentPageNum != $totalRows){
-                $html .= $this->_setNextLink($currentPageNum + 1);
-            }             
+        
+        if(($curNum + $this->_interval) < $totalNum){
+            $html .= '<a href="' . $link .  $totalNum . '" class="last">' . $this->_lastLink . '</a>';
         }
-        else {
-            $html .= $this->_setPrevLink($currentPageNum - 1); 
-            $html .= $this->_setSimpleLink(1);
-            $html .= $this->_setEllipsis();
-
-            $x = floor($countLinks / 2);
-            for($i = $currentPageNum - $x; $i <= $currentPageNum + $x; $i++){
-                if($i == $currentPageNum){
-                    $html .= $this->_setCurrentLink($i);
-                } else {
-                    $html .= $this->_setSimpleLink($i);
-                }
-            }
-
-            $html .= $this->_setEllipsis();
-            $html .= $this->_setSimpleLink($pageCount);
-            $html .= $this->_setNextLink($currentPageNum + 1);
+        
+        if($html != ''){
+            return '<div class="pagination">' . $html . '</div>';
         }
         
         return $html;
-    }
-    
-    private function _setPrevLink($num){
-        return '<a href="' . $this->_url . '?page=' . $num . '">' . $this->_prevLinkTitle . '</a>';
-    }
-
-    private function _setNextLink($num){
-        return '<a href="' . $this->_url . '?page=' . $num . '">' . $this->_nextLinkTitle . '</a>';
-    }
-    
-    private function _setCurrentLink($num){
-        return '<strong>' . $num . '</strong>';
-    }
-    
-    private function _setSimpleLink($num){
-        return '<a href="' . $this->_url . '?page=' . $num . '">' . $num . '</a>';
-    }
-
-    private function _setEllipsis(){
-        return '<span>...</span>';
     }
 }
